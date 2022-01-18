e8fe25586da46a6202772a1cf8cfda0a485e24d6
AJV11 <90346121+AJV11@users.noreply.github.com>
2022-01-17 19:39:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,10 @@ function setup() {
 }
 
 function draw() {
-  background(255, 166, 0);
+  
+  if (mouseIsPressed) {
+    background(0);
+  }
   // Update and display object
   w.update();
   w.display();
@@ -22,7 +25,18 @@ function Walker() {
 
   this.display = function() {
     // Draw Walker as circle
-    fill(random(255), random(255), random(255));
-    ellipse(this.pos.x, this.pos.y, 50, 50);
+    if (key == 'r') {
+      fill(255, 0, 0);
+    }
+    if (key == 'g') {
+      fill(0, 255, 0);
+    }
+    if (key == 'b') {
+      fill(0, 0, 255);
+    }
+    if (key == 'a') {
+      fill(random(255), random(255), random(255));
+    }
+    ellipse(this.pos.x, this.pos.y, 30, 30);
   }
 }
