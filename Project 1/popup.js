1bb92051f228e5d95210a8526c38e979a60a544f
AJV11 <90346121+AJV11@users.noreply.github.com>
2022-01-24 18:24:46 -0500

    Update popup.js

--- Project 1/popup.js
+++ Project 1/popup.js
@@ -3,8 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const bg = chrome.extension.getBackgroundPage()
     Object.keys(bg.x).forEach(function (url) {
         const div = document.createElement('div')
-        div.textContent = `${url}: # of X's- ${bg.x[url]}, # of E's- ${bg.e[url]}`
+        const divx = document.createElement('div')
+        const dive = document.createElement('div')
+        const br = document.createElement('br')
+        div.textContent = `Webpage: ${url}`
         document.body.appendChild(div)
+        document.body.appendChild(br)
+        divx.textContent = `# of X's - ${bg.x[url]}`
+        document.body.appendChild(divx)
+        document.body.appendChild(br)
+        dive.textContent = `# of E's - ${bg.e[url]}`
+        document.body.appendChild(dive)
+        document.body.appendChild(br)
     })
     
 }, false)
