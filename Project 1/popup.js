e5d8558f8a1a23b0f3ee5e20b0eea439ab8084c2
AJV11 <90346121+AJV11@users.noreply.github.com>
2022-01-25 19:38:33 -0500

    Update popup.js

--- Project 1/popup.js
+++ Project 1/popup.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
         const div = document.createElement('div')
         const divx = document.createElement('div')
         const dive = document.createElement('div')
+        const divd = document.createElement('div')
         const br = document.createElement('br')
+        let dif = `${bg.e[url]}` - `${bg.x[url]}`
         div.textContent = `Webpage: ${url}`
         document.body.appendChild(div)
         document.body.appendChild(br)
@@ -15,8 +17,12 @@ document.addEventListener('DOMContentLoaded', function () {
         dive.textContent = `# of E's - ${bg.e[url]}`
         document.body.appendChild(dive)
         document.body.appendChild(br)
+        divd.textContent = `Difference(E-X): `+dif
+        document.body.appendChild(divd)
+        document.body.appendChild(br)
     })
     
 }, false)
 
 // X icon image: https://www.pngall.com/wp-content/uploads/5/X-Letter-PNG.png
+// Youtube video reference: https://www.youtube.com/watch?v=Ipa58NVGs_c
