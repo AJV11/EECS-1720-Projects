8d458f393a4306e1c61ed9aa60485f6f7728d6b2
AJV11 <90346121+AJV11@users.noreply.github.com>
2022-01-25 19:37:05 -0500

    Update content.js

--- Project 1/content.js
+++ Project 1/content.js
@@ -1,15 +1,9 @@
 const re = new RegExp('x', 'gi')
-const matches =
-document.documentElement.innerHTML.match(re)
-chrome.runtime.sendMessage({
-    url: window.location.href,
-    count: matches.length
-})
-
+const matches = document.documentElement.innerHTML.match(re)
 const re2 = new RegExp('e', 'gi')
-const matches2 =
-document.documentElement.innerHTML.match(re2)
+const matches2 = document.documentElement.innerHTML.match(re2)
 chrome.runtime.sendMessage({
     url: window.location.href,
+    count: matches.length,
     count2: matches2.length
 })
