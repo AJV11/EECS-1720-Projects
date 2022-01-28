da3f059149c9ba99aac14cc7f69468526beb4c1a
AJV11 <90346121+AJV11@users.noreply.github.com>
2022-01-28 17:31:29 -0500

    Delete content.js

--- Project 1/content.js
+++
@@ -1,9 +0,0 @@
-const re = new RegExp('x', 'gi')
-const matches = document.documentElement.innerHTML.match(re)
-const re2 = new RegExp('e', 'gi')
-const matches2 = document.documentElement.innerHTML.match(re2)
-chrome.runtime.sendMessage({
-    url: window.location.href,
-    count: matches.length,
-    count2: matches2.length
-})
